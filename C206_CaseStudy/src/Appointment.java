14d01d3bf38fbbb38570a7cba66c6580804d645f
21023498 <21023498@10.174.114.190>
2022-08-04 15:22:28 +0800

    Daryl Appointment

---
+++ C206_CaseStudy/src/Appointment.java
@@ -0,0 +1,29 @@
+
+public class Appointment {
+	
+	private String date;
+	private String time;
+	
+	public Appointment(String date, String time) {
+		this.date = date;
+		this.time = time;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+}
--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,42 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	ArrayList<Appointment> appointmentList= new ArrayList<Appointment>();
 	}
+	
+	//================================= Option 1 View items (CRUD- Read) =================================
+	public static String retrieveAllAppointment(ArrayList<Appointment> appointmentList) {
+		String output = "";
 
+		for (int i = 0; i < appointmentList.size(); i++) {
+
+			output += String.format("%-20s %-20s\n", appointmentList.get(i).getDate(),appointmentList.get(i).getTime()) 
+		}
+		return output;
+	}
+	public static void viewAllCamcorder(ArrayList<Appointment> appointmentList) {
+		String output = String.format("%-20s %-20s\n", "DATE", "TIME");
+		 output += retrieveAllAppointment(appointmentList);	
+		System.out.println(output);
+	}
+	
+	//================================= Option 2 Add an item (CRUD - Create) =================================
+		public static Appointment inputAppointment() {
+			String date = Helper.readString("Enter date > ");
+			String time= Helper.readString("Enter time > ");
+
+			Appointment ap = new Appointment(date, time);
+			return ap;
+			
+		}
+		public static void addCamcorder(ArrayList<Appointment> appointmentList, Appointment ap) {
+			
+			appointmentList.add(ap);
+			
+		}
 }
+
