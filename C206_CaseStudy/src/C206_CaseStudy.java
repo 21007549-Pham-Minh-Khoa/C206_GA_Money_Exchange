fbf66421d52484d8eb08749ede216dd99a9f308b
21023498 <21023498@10.174.114.190>
2022-08-04 17:33:03 +0800

    h

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -8,28 +8,31 @@ public class C206_CaseStudy {
 	ArrayList<Appointment> appointmentList= new ArrayList<Appointment>();
 	}
 	
-	//================================= Option 1 View items (CRUD- Read) =================================
+	//================================= Option 1 View appointment (CRUD- Read) =================================
 	public static String retrieveAllAppointment(ArrayList<Appointment> appointmentList) {
 		String output = "";
 
 		for (int i = 0; i < appointmentList.size(); i++) {
 
-			output += String.format("%-20s %-20s\n", appointmentList.get(i).getDate(),appointmentList.get(i).getTime()) 
+			output += String.format("%-20s %-20s %-20s %-20s %-20s\n", appointmentList.get(i).getDate(),appointmentList.get(i).getTime(), appointmentList.get(i).getName(),appointmentList.get(i).getUsername(), appointmentList.get(i).getEmail()); 
 		}
 		return output;
 	}
 	public static void viewAllCamcorder(ArrayList<Appointment> appointmentList) {
-		String output = String.format("%-20s %-20s\n", "DATE", "TIME");
+		String output = String.format("%-20s %-20s %-20s %-20s %-20s\n", "DATE", "TIME", "NAME", "USERNAME", "EMAIL");
 		 output += retrieveAllAppointment(appointmentList);	
 		System.out.println(output);
 	}
 	
-	//================================= Option 2 Add an item (CRUD - Create) =================================
+	//================================= Option 2 Add appointment (CRUD - Create) =================================
 		public static Appointment inputAppointment() {
 			String date = Helper.readString("Enter date > ");
 			String time= Helper.readString("Enter time > ");
+			String name = Helper.readString("Enter name > ");
+			String username = Helper.readString("Enter username > ");
+			String email = Helper.readString("Enter email > ");
 
-			Appointment ap = new Appointment(date, time);
+			Appointment ap = new Appointment(date, time, name, username, email);
 			return ap;
 			
 		}
@@ -37,6 +40,36 @@ public class C206_CaseStudy {
 			
 			appointmentList.add(ap);
 			
+		}
+
+	//================================= Option 3 Delete appointment (CRUD - Delete) =================================
+		public static boolean doDeleteAppointments(ArrayList<Appointment> appointmentList, String id) {
+
+		    boolean isDeleted = false;
+
+//		    id = Helper.readString("Enter id > ");
+
+		    for (int i = 0; i < appointmentList.size(); i++) {
+		      if (appointmentList.get(i).getId() == id) {
+		        isDeleted = true;
+		      }
+		    }
+		    return isDeleted;
+		  }
+
+		  public static void deleteAppointment(ArrayList<Appointment> appointmentList) {
+		    AppointmentMain.viewAllFeedback(appointmentList);
+
+		    String id = Helper.readString("Enter id > ");
+		    Boolean isDeleted = doDeleteAppointment(appointmentList, id);
+		    if (isDeleted == false) {
+		      System.out.println("Invalid ID entered");
+		    } else {
+		      System.out.println("ID" + id + "deleted");
+
+		    }
+		  }
+
 		}
 }
 
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,18 +1,68 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class C206_CaseStudyTest {
+	private Appointment app1;
+	private Appointment app2;
+	private ArrayList<Appointment> appointmentList;
+	
 
 	@Before
 	public void setUp() throws Exception {
+		app1 = new Appointment ("01/01/20" , "22:30" , "Daryl" , "DarylU" , "daryl@gmail.com");
+		app2 = new Appointment ("01/02/20" , "22:30" , "Khoa" , "KhoaU" , "Khoa@gmail.com");
+		appointmentList = new ArrayList <Appointment>();
 	}
+	
+	public void testAddAppointment() {
+	assertNotNull("Test if there is valid Appointment arraylist to add to", appointmentList);
+	
+	//Given an empty list, after adding 1 item, the size of the list is 1
+	C206_CaseStudy.addAppointment(appointmentList, app1);		
+	assertEquals("Test if that appointment arraylist size is 1?", 1, appointmentList.size());
+	
+	//The item just added is as same as the first item of the list
+	assertSame("Test that appointment is added same as 1st item of the list?", app1, appointmentList.get(0));
+	
+	//Add another item. test The size of the list is 2?
+	C206_CaseStudy.addAppointment(appointmentList, app2);
+	assertEquals("Test that appointment arraylist size is 2?", 2, appointmentList.size());
+	assertSame("Test that appointment is added same as 3rd item of the list?", app2, appointmentList.get(2));
+	}
+	
+	public void testRetrieveAllAppointment() {
+		assertNotNull("Test if there is valid Chromebook arraylist to add to", appointmentList);
+		
+		//test if the list of appointment retrieved from the SourceCentre is empty
+		String allappointment= C206_CaseStudy.retrieveAllAppointment(appointmentList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllappointmentlist", testOutput, allappointment);
+				
+		//Given an empty list, after adding 2 items, test if the size of the list is 2
+		C206_CaseStudy.addAppointment(appointmentList, app1);
+		C206_CaseStudy.addAppointment(appointmentList, app2);
+		assertEquals("Test if that appointment arraylist size is 2?", 2, appointmentList.size());
+		
+		//test if the expected output string same as the list of appointment retrieved from the SourceCentre
+		allappointment= C206_CaseStudy.retrieveAllAppointment(appointmentList);
 
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n","CC0011", "Nikon HDSLR", "Yes", "", 40);
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n","CC0012", "Sony DSC-RX100M7", "Yes", "", 20);
+	
+		assertEquals("Check that ViewAllappointmentlist", testOutput, allappointment);
+	}
+	
+	
+	public void test
 	@After
 	public void tearDown() throws Exception {
-	}
+	
 
 	@Test
 	public void c206_test() {
