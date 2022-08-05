fe088deb8faac481ae216a33cc2fb61dabf5a4d6
21007549 <21007549@Khoa-Lenovo>
2022-08-05 09:29:07 +0800

    add employee, add customer, view employee, view customer

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,90 +1,170 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class C206_CaseStudy {
-
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		employee emp1 = new employee("John", "John@0203200","John@blc.com","J123A");
+		employee emp2 = new employee("James", "jAmEss$$1998","James@blc.com","J224D");
+		employee emp3 = new employee("Jenny", "Jenny$052002","Jenny@blc.com","J689F");
+		
+		customer cust1 = new customer("Jackson","Jacky@04","Jackson2004@gmail.com","Jackson2004");
+		customer cust2 = new customer("Hanna","Hanna@97","Hanna.SG@gmail.com","HannaKana");
+		customer cust3 = new customer("AnyName","AnyName@00","AnyEmail@gmail.com","AnyUsername");
+		ArrayList <employee> employeeList = new ArrayList<employee>();
+		ArrayList <customer> customerList = new ArrayList<customer>();
 		
 	ArrayList<Appointment> appointmentList= new ArrayList<Appointment>();
 	}
 	
-	//================================= Option 1 View items (CRUD- Read) =================================
-//	public static String retrieveAllAppointment(ArrayList<Appointment> appointmentList) {
-//		String output = "";
-//
-//		for (int i = 0; i < appointmentList.size(); i++) {
-//
-//			output += String.format("%-20s %-20s\n", appointmentList.get(i).getDate(),appointmentList.get(i).getTime()) 
-//		}
-//		return output;
-//	}
-//	public static void viewAllCamcorder(ArrayList<Appointment> appointmentList) {
-//		String output = String.format("%-20s %-20s\n", "DATE", "TIME");
-//		 output += retrieveAllAppointment(appointmentList);	
-//		System.out.println(output);
-//	}
-//	
-//	//================================= Option 2 Add an item (CRUD - Create) =================================
-//		public static Appointment inputAppointment() {
-//			String date = Helper.readString("Enter date > ");
-//			String time= Helper.readString("Enter time > ");
-//			String username = Helper.red
-//
-//			Appointment ap = new Appointment(name,email,date, time);
-//			return ap;
-//			
-//		}
-//		public static void addAppointment(ArrayList<Appointment> appointmentList, Appointment ap) {
-//			
-//			appointmentList.add(ap);
-//			
-//		}
-
-<<<<<<< HEAD
-		for (int i = 0; i < appointmentList.size(); i++) {
-
-			output += String.format("%-20s %-20s\n", appointmentList.get(i).getDate(),appointmentList.get(i).getTime()); 
-		}
-		return output;
-	}
-	public static void viewAllCamcorder(ArrayList<Appointment> appointmentList) {
-		String output = String.format("%-20s %-20s\n", "DATE", "TIME");
-		 output += retrieveAllAppointment(appointmentList);	
-		System.out.println(output);
+	
+	public static employee inputEmployee() {
+		employee emp = null;
+		String fname = Helper.readString("Enter your first name: ");
+		String lname = Helper.readString("Enter your last name: ");
+		String name = fname+lname;
+		
+		String email = Helper.readString("Enter your orgs email: ");
+		String password = Helper.readString("Set Your Password: ");
+		
+		Random ID = new Random ();
+		int i = ID.nextInt();
+		String employeeID = fname+i;
+		
+		emp = new employee (name,password,email,employeeID);
+		
+		return emp;
 	}
 	
-	//================================= Option 2 Add an item (CRUD - Create) =================================
-		public static Appointment inputAppointment() {
-			String date = Helper.readString("Enter date > ");
-			String time= Helper.readString("Enter time > ");
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+		public static customer inputCustomer() {
+			customer cust = null;
+			String fname = Helper.readString("Enter your first name: ");
+			String lname = Helper.readString("Enter your last name: ");
+			String name = fname+lname;
+			
+			String email = Helper.readString("Enter your email: ");
+			String password = Helper.readString("Set your Password: ");
+			
+			String username = Helper.readString("Enter your username: ");
+			
+			cust = new customer(name,password,email,username);
+			
+			return cust;
+		}
 
-			Appointment ap = new Appointment(date, time);
-			return ap;
-=======
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		public static void AddCustomer(ArrayList<customer> customerList, customer cust) {
-			// TODO Auto-generated method stub
->>>>>>> branch 'master' of https://github.com/21007549-Pham-Minh-Khoa/C206_GA_Team3.git
-			
+			customerList.add(cust);
 		}
 		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////		
 		public static void AddEmployee(ArrayList<employee> employeeList, employee emp) {
-			
+			employeeList.add(emp);
 		}
 		
-		public static void ViewCustomer(ArrayList<customer> customerList) {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		public static String ViewCustomer(ArrayList<customer> customerList, String srch_Username) {
+			//srch_Username = Helper.readString("Enter username to search: ");
+			boolean isFound = findCustomer(customerList,srch_Username);
+			String output ="";
 			
+			if (isFound == true) {
+				for (customer i : customerList) {
+					if (srch_Username.equals(i.getUsername())) {
+						output = String.format("Username: %s\n"
+								+ "Name: %s\n"
+								+ "Password: %s\n"
+								+ "Email: %s", i.getUsername(), i.getName(), i.getPassword(), i.getEmail());
+						break;
+					}
+				}
+			} else {
+				output = "Username not found.";
+			}
+			return output;
 		}
 		
-		public static void ViewEmployee(ArrayList<employee> employeeList) {
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		public static String ViewEmployee(ArrayList<employee> employeeList,String employeeID) {
+			String output = "";
+			boolean isFound = findEmployee(employeeList,employeeID);
+			if (isFound == true) {
+				for (employee i : employeeList) {
+					if (employeeID.equals(i.getEmployeeID())) {
+						output = String.format("Employee ID: %s\n"
+								+ "Name: %s\n"
+								+ "Password: %s\n"
+								+ "Email: %s",i.getEmployeeID(), i.getName(), i.getPassword(),i.getEmail() );
+						break;
+					}
+				}
+			} else {
+				output = "Employee not found.";
+			}
 			
+			return output;
 		}
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		
 		public static void DeleteCustomer(ArrayList<customer> customerList,customer cust) {
 			
 		}
 		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		public static void DeleteEmployee(ArrayList<employee> employeeList, employee emp) {
 			
+		}
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		public static void LoginCustomer(ArrayList<customer> customerList, String username, String email, String password) {
+			
+		}
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		public static void LoginEmployee(ArrayList<employee> employeeList, String employeeId, String email,String password) {
+			
+		}
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		public static boolean findCustomer(ArrayList<customer> customerList, String srch_Username) {
+			boolean isFound = false;
+			for (customer i : customerList) {
+				if (i.getUsername().equals(srch_Username)) {
+					isFound = true;
+					break;
+				}
+			}
+			
+			
+			return isFound;
+		}
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		public static boolean findEmployee(ArrayList<employee> employeeList, String employeeID) {
+			boolean isFound = false;
+			for (employee i : employeeList) {
+				if (i.getEmployeeID().equals(employeeID)) {
+					isFound = true;
+					break;
+				}
+			}
+			
+			
+			return isFound;
 		}
 }
 
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -10,9 +10,11 @@ public class C206_CaseStudyTest {
 	private employee emp1;
 	private employee emp2;
 	private employee emp3;
+	private employee emp4;
 	private customer cust1;
 	private customer cust2;
 	private customer cust3;
+	private customer cust4;
 	
 	private ArrayList <employee> employeeList;
 	private ArrayList <customer> customerList;
@@ -22,17 +24,28 @@ public class C206_CaseStudyTest {
 		emp1 = new employee("John", "John@0203200","John@blc.com","J123A");
 		emp2 = new employee("James", "jAmEss$$1998","James@blc.com","J224D");
 		emp3 = new employee("Jenny", "Jenny$052002","Jenny@blc.com","J689F");
+		emp4 = new employee(null, null, null, null);
 		
 		cust1 = new customer("Jackson","Jacky@04","Jackson2004@gmail.com","Jackson2004");
-		cust2 = new customer("Jackson","Jacky@04","Jackson2004@gmail.com","Jackson2004");
-		cust3 = new customer("Jackson","Jacky@04","Jackson2004@gmail.com","Jackson2004");
+		cust2 = new customer("Hanna","Hanna@97","Hanna.SG@gmail.com","HannaKana");
+		cust3 = new customer("AnyName","AnyName@00","AnyEmail@gmail.com","AnyUsername");
+		cust4 = new customer(null, null, null,null);
 		
 		employeeList = new ArrayList<employee>();
 		customerList = new ArrayList<customer>();
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		cust1 = null;
+		cust2 = null;
+		cust3 = null;
+		emp1 = null;
+		emp2 = null;
+		emp3 = null;
+		employeeList = null;
+		customerList = null;
 	}
 
 	@Test
@@ -41,17 +54,81 @@ public class C206_CaseStudyTest {
 		assertNotNull("Test if customerList is empty",customerList);
 		// Given an empty list, after adding 1 item, the size of the list is 1
 		C206_CaseStudy.AddCustomer(customerList, cust1);
-		assertEquals("Test if that Camcorder arraylist size is 1?", 1, camcorderList.size());
+		assertEquals("Test if that Customer arraylist size is 1?", 1, customerList.size());
 
 		// The item just added is as same as the first item of the list
-		assertSame("Test that Camcorder is added same as 1st item of the list?", cc1, camcorderList.get(0));
+		assertSame("Test that the Customer is added same as 1st item of the list?", cust1, customerList.get(0));
 
 		// Add another item. test The size of the list is 2?
-		ResourceCentre.addCamcorder(camcorderList, cc2);
-		ResourceCentre.addCamcorder(camcorderList, cc3);
-		assertEquals("Test that Camcorder arraylist size is 3?", 3, camcorderList.size());
-		assertSame("Test that Camcorder is added same as 3rd item of the list?", cc3, camcorderList.get(2));
+		C206_CaseStudy.AddCustomer(customerList, cust2);
+		C206_CaseStudy.AddCustomer(customerList, cust3);
+		assertEquals("Test that Customer arraylist size is 3?", 3, customerList.size());
+		assertSame("Test that Customer is added same as 3rd item of the list?", cust3, customerList.get(2));
 
+	}
+	@Test
+	public void testAddEmployee() {
+		//fail("Not yet implemented"); 
+		assertNotNull("Test if emplloyeeList is empty",employeeList);
+		// Given an empty list, after adding 1 item, the size of the list is 1
+		C206_CaseStudy.AddEmployee(employeeList, emp1);
+		assertEquals("Test if that Employee arraylist size is 1?", 1, employeeList.size());
+
+		// The item just added is as same as the first item of the list
+		assertSame("Test that the Employee is added same as 1st item of the list?", emp1, employeeList.get(0));
+
+		// Add another item. test The size of the list is 2?
+		C206_CaseStudy.AddEmployee(employeeList, emp2);
+		C206_CaseStudy.AddEmployee(employeeList, emp3);
+		assertEquals("Test that Employee arraylist size is 3?", 3, employeeList.size());
+		assertSame("Test that Employee is added same as 3rd item of the list?", emp3, employeeList.get(2));
+
+	}
+	@Test
+	public void testViewCustomer(){
+		assertNotNull("Test if there is a valid Customer ArrayList to add to", customerList);
+		
+		String srchUsername="";
+		String cust = C206_CaseStudy.ViewCustomer(customerList,srchUsername);
+		String testOutput = "Username not found.";
+		assertEquals("Check that ViewCustomer run",testOutput,cust);
+		
+		C206_CaseStudy.AddCustomer(customerList, cust1);
+		C206_CaseStudy.AddCustomer(customerList, cust2);
+		assertEquals("Test that customerList have 2 customer",2,customerList.size());
+		
+		srchUsername = "Jackson2004";
+		cust = C206_CaseStudy.ViewCustomer(customerList,srchUsername);
+		testOutput = String.format("Username: %s\n"
+				+ "Name: %s\n"
+				+ "Password: %s\n"
+				+ "Email: %s", "Jackson2004","Jackson","Jacky@04","Jackson2004@gmail.com");
+		
+		assertEquals("Test that ViewCustomer run",testOutput,cust);
+		
+	}
+	@Test
+	public void testViewEmployee(){
+		assertNotNull("Test if there is a valid Employee ArrayList to add to", employeeList);
+		
+		String srchID="";
+		String emp = C206_CaseStudy.ViewEmployee(employeeList,srchID);
+		String testOutput = "Employee not found.";
+		assertEquals("Check that ViewEmployee run",testOutput,emp);
+		
+		C206_CaseStudy.AddEmployee(employeeList, emp1);
+		C206_CaseStudy.AddEmployee(employeeList, emp2);
+		assertEquals("Test that employeeList have 2 employee",2,employeeList.size());
+		
+		srchID = "J123A";
+		emp = C206_CaseStudy.ViewEmployee(employeeList,srchID);
+		testOutput = String.format("Employee ID: %s\n"
+				+ "Name: %s\n"
+				+ "Password: %s\n"
+				+ "Email: %s","J123A","John", "John@0203200","John@blc.com" );
+		
+		assertEquals("Test that ViewEmployee run",testOutput,emp);
+		
 	}
 
 }
